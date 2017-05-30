eae697d6c333a1cc5ba4e7185089bef88ac102ba
Olly Shaw <ollyjshaw@gmail.com>
2017-05-30 10:20:14 +0100

    responds to welcome route

--- test/controllers/WelcomeControllerSpec.scala
+++ test/controllers/WelcomeControllerSpec.scala
@@ -12,5 +12,11 @@ class WelcomeControllerSpec extends PlaySpec with GuiceOneAppPerTest {
       val result = controller.welcome().apply(FakeRequest(GET, "/foo"))
       status(result) mustBe OK
     }
+    "respond to the /welcome url" in {
+      // Need to specify Host header to get through AllowedHostsFilter
+      val request = FakeRequest(GET, "/welcome").withHeaders("Host" -> "localhost")
+      val home = route(app, request).get
+      status(home) mustBe OK
+    }
   }
 }
