3d5ea5b567bb2ea85d557bc39ef33350f201736a
Olly Shaw <ollyjshaw@gmail.com>
2017-06-01 11:46:03 +0100

    time based greeting

--- app/service/GreetingService.scala
+++ app/service/GreetingService.scala
@@ -5,7 +5,14 @@ import java.util.Calendar
 import com.google.inject.ImplementedBy
 
 class RealGreetingService extends GreetingService {
-  def greeting: String = ???
+  def greeting: String =  {
+    val now = Calendar.getInstance()
+    val currentHour = now.get(Calendar.HOUR_OF_DAY)
+    if (currentHour < 12)
+      "Good morning!"
+    else
+      "Good afternoon!"
+  }
 }
 
 @ImplementedBy(classOf[RealGreetingService])
